34e92c8008554f034877938e8b796fef567b1003
ssadaranga <ssadarangas@gmail.com>
2020-05-24 13:08:42 +0530

    first commit

---
+++ src/main/java/utils/JsonReaderHelper.java
@@ -0,0 +1,18 @@
+package utils;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import data.LoginData;
+import jdk.nashorn.internal.parser.JSONParser;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class JsonReaderHelper {
+    public static LoginData[] getLoginData() throws FileNotFoundException {
+        JsonElement root = new JsonParser().parse(new FileReader(System.getProperty("user.dir") + "/src/main/resources/data/loginData.json"));
+        Gson gson = new Gson();
+        LoginData[] loginData = gson.fromJson(root, LoginData[].class);
+        return loginData;
+    }
+}
